6102e31dca2425e70dade161290b380e8f169210
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-06-19 19:11:24 +0700

    Add project files.

---
+++ PurseManager/Model/CategoryModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurseManager.Model
+{
+    public class CategoryModel
+    {
+        private int count { get; set; }
+        //private int 
+
+        public List<string> listOfExpenditures = new List<string>
+        {
+            "Auto",
+            "Medicine",
+            "Sport",
+            "Family"
+        };
+
+        public List<string> listOfReciepts = new List<string>
+        {
+            "Salary",
+            "Dividends",
+            "Illegal market :)"
+        };
+
+        public List<OperationModel> GetRecieptList(int total, List<string> list, int length)
+        {
+            List<OperationModel> result = new List<OperationModel>();
+            count = list.Count - 1;
+            for (int i = 0; i < total; i++)
+            {
+                result.Add(GetReciept((i + 1), list, length));
+                count--;
+            }
+            return result;
+        }
+
+        private OperationModel GetReciept(int id, List<string> data, int length)
+        {
+            OperationModel result = new OperationModel();
+            result.NameOfChange = GetItemOfList(data);
+            return result;
+        }
+
+        private string GetItemOfList(List<string> data)
+        {
+            return data[count];
+        }
+    }
+}
---
+++ PurseManager/Model/OperationModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurseManager.Model
+{
+    public class OperationModel
+    {
+        public int Id { get; set; }
+        public decimal ValueOfMoney { get; set; }
+        public string NameOfChange { get; set; }
+        public string Note { get; set; }
+        public string FullName
+        {
+            get { return NameOfChange; }
+        }
+        public OperationModel()
+        {
+
+        }
+        public OperationModel(string valueOfMoney, string nameOfChange, string note)
+        {
+            NameOfChange = nameOfChange;
+            Note = note;
+
+            decimal amountOfMoney = 0;
+            decimal.TryParse(valueOfMoney, out amountOfMoney);
+            ValueOfMoney = amountOfMoney;
+        }
+    }
+}
